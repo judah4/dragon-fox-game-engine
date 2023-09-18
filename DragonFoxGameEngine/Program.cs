e5dd5338e057df3fdb8757e2c387238295bb68f2
Judah Perez <me@judahperez.com>
2023-09-17 18:52:26 -0700

    basic file logging

--- DragonFoxGameEngine/Program.cs
+++ DragonFoxGameEngine/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using DragonFoxGameEngine.Core;
+using NReco.Logging.File;
+using System.IO;
+using Microsoft.Extensions.Primitives;
+using System;
 
 namespace DragonFoxGameEngine
 {
@@ -18,6 +22,26 @@ namespace DragonFoxGameEngine
                 options.IncludeScopes = false;
                 options.SingleLine = true;
                 options.TimestampFormat = "hh:mm:ss ";
+            });
+            //https://github.com/nreco/logging
+            builder.Logging.AddFile("app.log", fileLoggerOpts => {
+                fileLoggerOpts.Append = true;
+                fileLoggerOpts.FormatLogEntry = (msg) => {
+                    var sb = new System.Text.StringBuilder();
+                    sb.Append(DateTime.Now.ToString("o"));
+                    sb.Append(" ");
+                    sb.Append(msg.LogLevel);
+                    sb.Append(" ");
+                    sb.Append(msg.LogName);
+                    sb.Append(" ");
+                    sb.Append(msg.EventId.Id);
+                    sb.Append(" ");
+                    sb.Append(msg.Message);
+                    sb.Append(" ");
+                    sb.Append(msg.Exception?.ToString());
+                    sb.Append(" ");
+                    return sb.ToString();
+                };
             });
             builder.Configuration.AddUserSecrets<Program>();
             builder.Services.AddHostedService<EngineHostedService>();
