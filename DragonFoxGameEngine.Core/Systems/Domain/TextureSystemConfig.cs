9c21a834df55674a81a6ba0cfd10e18e12f89a1f
Judah Perez <me@judahperez.com>
2023-10-14 23:34:59 -0700

    Stubbing out texture system

---
+++ DragonFoxGameEngine.Core/Systems/Domain/TextureSystemConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace DragonGameEngine.Core.Systems.Domain
+{
+    public record TextureSystemConfig
+    {
+        public readonly uint MaxTextureCount;
+
+        public TextureSystemConfig(uint maxTextureCount)
+        {
+            MaxTextureCount = maxTextureCount;
+        }
+    }
+}
---
+++ DragonFoxGameEngine.Core/Systems/TextureSystem.cs
@@ -0,0 +1,35 @@
+﻿using DragonGameEngine.Core.Resources;
+using DragonGameEngine.Core.Systems.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonGameEngine.Core.Systems
+{
+    public sealed class TextureSystem
+    {
+        public const string DEFAULT_TEXTURE_NAME = "default";
+
+        public void Initialize(ulong memoryRequirement, Span<byte> state, TextureSystemConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown(Span<byte> state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Texture Acquire(string name, bool autoRelease)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Texture Release(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
