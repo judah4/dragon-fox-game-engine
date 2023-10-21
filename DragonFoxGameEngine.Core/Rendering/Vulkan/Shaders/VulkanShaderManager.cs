eb683b7b578795a16c3860e7458f5b950d1e7479
Judah Perez <me@judahperez.com>
2023-10-21 16:26:57 -0700

    Load shaders from the asset folder

--- DragonFoxGameEngine.Core/Rendering/Vulkan/Shaders/VulkanShaderManager.cs
+++ DragonFoxGameEngine.Core/Rendering/Vulkan/Shaders/VulkanShaderManager.cs
@@ -1,6 +1,8 @@
 ﻿using DragonGameEngine.Core.Exceptions.Vulkan;
 using DragonGameEngine.Core.Rendering.Vulkan.Domain;
 using DragonGameEngine.Core.Rendering.Vulkan.Domain.Shaders;
+using DragonGameEngine.Core.Resources;
+using DragonGameEngine.Core.Systems;
 using Silk.NET.Core.Native;
 using Silk.NET.Vulkan;
 using System.IO;
@@ -9,12 +11,22 @@ namespace DragonGameEngine.Core.Rendering.Vulkan.Shaders
 {
     public unsafe sealed class VulkanShaderManager
     {
+
+        private readonly ResourceSystem _resourceSystem;
+        public VulkanShaderManager(ResourceSystem resourceSystem) 
+        {
+            _resourceSystem = resourceSystem;
+        }
+
         public VulkanShaderStage CreateShaderModule(VulkanContext context, string shaderName, string stageTypeName, ShaderStageFlags shaderStageFlags, int index)
         {
+
             //Build file name.
             var fileName = Path.Combine("Shaders/", $"{shaderName}.{stageTypeName}.spv");
 
-            var shaderCode = File.ReadAllBytes(fileName);
+            var resource = _resourceSystem.Load(fileName, ResourceType.Binary);
+
+            var shaderCode = (byte[])resource.Data;
 
             ShaderModuleCreateInfo createInfo = new()
             {
@@ -51,6 +63,8 @@ namespace DragonGameEngine.Core.Rendering.Vulkan.Shaders
                 Handle = shaderModule,
                 ShaderStageCreateInfo = shaderStageInfo,
             };
+
+            _resourceSystem.Unload(resource);
 
             return shaderStage;
         }
---
+++ DragonFoxGameEngine.Core/Systems/ResourceLoaders/BinaryResourceLoader.cs
@@ -0,0 +1,51 @@
+﻿using DragonGameEngine.Core.Exceptions;
+using DragonGameEngine.Core.Resources;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DragonGameEngine.Core.Systems.ResourceLoaders
+{
+    public sealed class BinaryResourceLoader : IResourceLoader
+    {
+        public ResourceType ResourceType => ResourceType.Binary;
+
+        public string TypePath => "";
+
+
+        private readonly ILogger _logger;
+        private readonly string _basePath;
+
+        public BinaryResourceLoader(ILogger logger, string basePath)
+        {
+            _logger = logger;
+            _basePath = basePath;
+        }
+
+        public Resource Load(string name)
+        {
+            return BinData(name);
+        }
+
+        public void Unload(Resource resource)
+        {
+            resource.SetData(0, Array.Empty<byte[]>());
+        }
+
+        private Resource BinData(string name)
+        {
+            //todo: try different extensions
+            var filePath = Path.Combine(_basePath, TypePath, name);
+
+            var bytes = File.ReadAllBytes(filePath);
+
+            if(bytes == null || bytes.Length < 1)
+            {
+                throw new ResourceException(name, $"Binary resource is empty!");
+            }
+
+            return new Resource(ResourceType, name, filePath, (ulong)bytes.Length, bytes);
+        }
+    }
+}
